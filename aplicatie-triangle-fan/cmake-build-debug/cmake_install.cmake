36b0383f89fb8fb60b48c86088a2bb6e102eb14b
eva.simpetru <eva.simpetru@esolutions.ro>
2024-01-19 18:31:16 +0200

    Rotating triangle fan

--- aplicatie-triangle-fan/cmake-build-debug/cmake_install.cmake
+++ aplicatie-triangle-fan/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/eva/CLionProjects/aplicatie-triangle-fan
+# Install script for directory: /home/eva/CLionProjects/GC/aplicatie-triangle-fan
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/eva/CLionProjects/aplicatie-triangle-fan/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/eva/CLionProjects/GC/aplicatie-triangle-fan/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
